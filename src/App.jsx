3a1a83e791aeb5fdefa172ae5ae3abd38505d361
meeckeytek001 <165794948+meeckeytek001@users.noreply.github.com>
2024-04-25 20:46:40 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + Release</h1>
      <Router>
         <Routes>
           <Route path="/"  element={<Home />} />
